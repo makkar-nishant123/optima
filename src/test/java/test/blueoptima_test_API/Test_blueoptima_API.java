38f5342d7c965966f77bd1b01f230de582f6a708
nmakkar@xavient.com <nmakkar@xavient.com>
2017-03-22 15:18:37 +0530

    Final Commit

--- src/test/java/test/blueoptima_test_API/Test_blueoptima_API.java
+++ src/test/java/test/blueoptima_test_API/Test_blueoptima_API.java
@@ -1,37 +1,19 @@
 package test.blueoptima_test_API;
 
 import org.testng.annotations.Test;
-
-import test.blueoptima_page.Pageclass;
-
+import com.jayway.restassured.http.ContentType;
 
 public class Test_blueoptima_API {
 	
-	//Initialsing Page-Class Object.
-	Pageclass pageclass = new Pageclass();
-	
-	/**
-	 * Method for performing test case.
-	 */
 	@Test
-	public void test_fourpoints()
-	{	
-		//Maximizing and navigating to URL.
-		pageclass.driver.manage().window().maximize();
-		pageclass.driver.get("https://uix-stage02.blueoptima.com/uix/login");
+	public void getdata_api() {
+		// API Name or Command .
+		String Command = "getData";
+
+		// Input (Can be in different format) we are passing it as String.
+		String Input = "";
 		
-		//Enter Username , Password and click on Login. (Assuming No session is already present).
-		pageclass.enter_user();
-		pageclass.enter_pass();
-		pageclass.click_Login();
-		
-		//Clicking on Developer Tab after waiting for it to load.
-		pageclass.devclickandvalidate();
-		
-		//Validating one graph.
-		pageclass.graph_verify();
-		
-		//Closing Browser- Keeping it commented to check the page loaded.. Can Uncomment for closing .
-//		pageclass.driver.close();	
-	}   
+		// Calling Method for API execution.
+		API_POC.ApiHelper.test_api(Command, Input, ContentType.JSON, null, null);
+	}
 }
--- src/test/java/test/blueoptima_test_UI/Test_blueoptima.java
+++ src/test/java/test/blueoptima_test_UI/Test_blueoptima.java
@@ -4,24 +4,34 @@ import org.testng.annotations.Test;
 
 import test.blueoptima_page.Pageclass;
 
-/**
- * Unit test for simple App.
- */
+
 public class Test_blueoptima {
 	
+	//Initialsing Page-Class Object.
 	Pageclass pageclass = new Pageclass();
+	
+	/**
+	 * Method for performing test case.
+	 */
 	@Test
 	public void test_fourpoints()
-	{	pageclass.driver.manage().window().maximize();
+	{	
+		//Maximizing and navigating to URL.
+		pageclass.driver.manage().window().maximize();
 		pageclass.driver.get("https://uix-stage02.blueoptima.com/uix/login");
+		
+		//Enter Username , Password and click on Login. (Assuming No session is already present).
 		pageclass.enter_user();
 		pageclass.enter_pass();
 		pageclass.click_Login();
-		pageclass.devclickandvalidate();
-		pageclass.select_checkbox_and_graph_verify();
-//		pageclass.driver.close();
 		
-	}
-	
-    
+		//Clicking on Developer Tab after waiting for it to load.
+		pageclass.devclickandvalidate();
+		
+		//Validating one graph.
+		pageclass.graph_verify();
+		
+		//Closing Browser- Keeping it commented to check the page loaded.. Can Uncomment for closing .
+//		pageclass.driver.close();	
+	}   
 }
